a27546013ac49c1d775a6250cc7ef691d3e6b42e
fniad <artkamproject@gmail.com>
2023-10-05 23:46:36 +0300

    added get_payments_this_user for serializer UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
+from payment.models import Payment
+from payment.serializers import PaymentSerializer
 from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    payments_this_user = SerializerMethodField()
+
+    def get_payments_this_user(self, user):
+        return PaymentSerializer(Payment.objects.filter(user=user), many=True).data
 
     class Meta:
         model = User
