185586fa129b9174ebd23e772c0bc8202f982666
fniad <artkamproject@gmail.com>
2024-01-16 19:47:14 +0300

    add cd

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,5 @@ poetry install &&
 poetry run python3 manage.py migrate &&
 # Сбор статики
 poetry run python3 manage.py collectstatic --no-input
+# docker-compose up --build -d
+# docker-compose exec app python manage.py migrate
