e430857468ce3645391135492fcbd88d9d36aa43
fniad <artkamproject@gmail.com>
2024-01-18 20:35:28 +0300

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 # Установка зависимостей
-poetry install
+echo "Установка зависимостей"
+#poetry install
 # Применение миграций
-poetry run python3 manage.py migrate
+#poetry run python3 manage.py migrate
 # Сбор статики
-poetry run python3 manage.py collectstatic --no-input
+#poetry run python3 manage.py collectstatic --no-input
 # docker-compose up --build -d
 # docker-compose exec app python manage.py migrate
